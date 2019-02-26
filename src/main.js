38c31e2e356f3bde5996658757cdd7ca5411a1ee
IPogorelova <inna.pogorelova05@gmail.com>
2019-02-26 14:04:30 +0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,46 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false;
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
+
+const requireComponent = require.context(
+  // Относительный путь до каталога компонентов
+  './components',
+  // Обрабатывать или нет подкаталоги
+  false,
+  // Регулярное выражение для определения файлов базовых компонентов
+  /Base[A-Z]\w+\.(vue|js)$/
+)
+
+requireComponent.keys().forEach(fileName => {
+  // Получение конфигурации компонента
+  const componentConfig = requireComponent(fileName)
+
+  // Получение имени компонента в PascalCase
+  const componentName = upperFirst(
+    camelCase(
+      // Удаление из начала `./` и расширения из имени файла
+      fileName.replace(/^\.\/(.*)\.\w+$/, '$1')
+    )
+  )
+
+  // Глобальная регистрация компонента
+  Vue.component(
+    componentName,
+    // Поиск опций компонента в `.default`, который будет существовать,
+    // если компонент экспортирован с помощью `export default`,
+    // иначе будет использован корневой уровень модуля.
+    componentConfig.default || componentConfig
+  )
+})
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
